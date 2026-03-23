8532bddd49631a970d150e5a7053e5da2afd2e78
GUJIU667 <gujiu667@gmail.com>
2026-03-23 16:37:58 +0800

    init: add 4-bit adder project

---
+++ adder_project/adder_tb.v
@@ -0,0 +1,20 @@
+`timescale 1ns/1ps  
+
+module adder_tb;
+reg [3:0] a, b;
+wire [4:0] sum;
+
+adder uut (
+    .a(a),
+    .b(b),
+    .sum(sum)
+);
+
+initial begin
+    a=0; b=0;
+    #10 a=3; b=5;
+    #10 a=7; b=8;
+    #10 a=15; b=1;
+    #10 $stop;
+end
+endmodule
